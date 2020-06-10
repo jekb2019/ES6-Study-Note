322efd21b45ac4dabae7ac8e222e09dccc0c70aa
Jason Ko <jekb2019@gmail.com>
2020-06-10 12:32:21 +1200

    Add new notes: module in ES6

---
+++ modules/main.js
@@ -0,0 +1,10 @@
+import {double, name} from './utils.js'; //import name variable, double function from util.js file
+
+console.log(double(5)); //10
+console.log(name);
+
+
+//Importing everything from utils.js
+// import * as Utils from './utils.js';
+// console.log(Utils.double(5));
+// console.log(Utils.name);
---
+++ modules/utils.js
@@ -0,0 +1,7 @@
+export const name = 'Dom';
+
+export function double(n){
+    return n*2;
+}
+
+// export (name, double); //if you do this, you do not need export in front of the variable and the function above
